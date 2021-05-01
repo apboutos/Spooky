f577bbfc38b400524d2218c3728166a740c86216
exophrenik <exophrenik@gmail.com>
2021-05-01 18:25:13 +0300

    Test Creation attempt

---
+++ core/test/java/LevelInitializerTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class LevelInitializerTest {
+
+    @Test
+    void initializeUnits() {
+        fail();
+    }
+}
