aeb706d0f8a48500949761505e5ee652ef64cd5a
exophrenik <exophrenik@gmail.com>
2021-05-02 09:08:10 +0300

    Speed implementation.

---
+++ core/src/com/apboutos/spooky/utilities/Speed.java
@@ -0,0 +1,16 @@
+package com.apboutos.spooky.utilities;
+
+import com.badlogic.gdx.math.Vector2;
+
+@SuppressWarnings("unused")
+public class Speed {
+
+    public static final Vector2 PLAYER_SPEED = new Vector2(6,6);
+    public static final Vector2 PLAYER_SPEED_BOOSTED = new Vector2();
+    public static final Vector2 FISH_SPEED = new Vector2(3,3);
+    public static final Vector2 FISH_SPEED_BOOSTED = new Vector2();
+    public static final Vector2 SHARK_SPEED = new Vector2(6,6);
+    public static final Vector2 SHARK_SPEED_BOOSTED = new Vector2(6,6);
+    public static final Vector2 BLOCK_SPEED = new Vector2(6,6);
+    public static final Vector2 BLOCK_SPEED_BOOSTED = new Vector2();
+}
