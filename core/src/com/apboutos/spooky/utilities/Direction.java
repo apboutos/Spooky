0d18361f05660715d63b19ca8d7ddcb92fb0c42e
exophrenik <exophrenik@gmail.com>
2021-05-02 22:06:53 +0300

    Added Direction manipulation methods.

--- core/src/com/apboutos/spooky/utilities/Direction.java
+++ core/src/com/apboutos/spooky/utilities/Direction.java
@@ -7,10 +7,38 @@ package com.apboutos.spooky.utilities;
  * @author exophrenik
  *
  */
+@SuppressWarnings("DuplicatedCode")
 public enum Direction {
 
 	UP,
 	DOWN,
 	LEFT,
-	RIGHT
+	RIGHT;
+
+	public static Direction reverseDirection(Direction direction){
+		switch (direction){
+			case UP: return DOWN;
+			case DOWN: return UP;
+			case LEFT: return RIGHT;
+			default: return LEFT;
+		}
+	}
+
+	public static Direction shiftClockwise(Direction direction){
+		switch (direction){
+			case UP: return RIGHT;
+			case DOWN: return LEFT;
+			case LEFT: return UP;
+			default: return DOWN;
+		}
+	}
+
+	public static Direction shiftCounterClockwise(Direction direction){
+		switch (direction){
+			case UP: return LEFT;
+			case DOWN: return RIGHT;
+			case LEFT: return DOWN;
+			default: return UP;
+		}
+	}
 }
