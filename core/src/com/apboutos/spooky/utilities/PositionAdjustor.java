9e7c9e62c431fa0595af91c5c2404c25b2620795
exophrenik <exophrenik@gmail.com>
2021-05-03 14:02:41 +0300

    PositionAdjustor refactored to work for all units. Behavior functionality removed.

--- core/src/com/apboutos/spooky/utilities/PositionAdjustor.java
+++ core/src/com/apboutos/spooky/utilities/PositionAdjustor.java
@@ -1,54 +1,38 @@
 package com.apboutos.spooky.utilities;
 
-import com.apboutos.spooky.units.Player;
+import com.apboutos.spooky.units.Enemy;
 import com.apboutos.spooky.units.Unit;
-import com.apboutos.spooky.units.enemy.Enemy;
-import com.badlogic.gdx.utils.TimeUtils;
 
 public class PositionAdjustor {
 
-    public void adjustPosition(Unit unit){
+    public void adjustPosition(Unit unit) {
 
-        if(unit instanceof Player || unit instanceof Enemy){
-
-            if(unit.isDead()){
-                unit.setMoving(false);
-
-                if (!unit.isDeathTimerStarted())
-                {
-                    startDeathTimer(unit);
-                }
+        if (unit.isMoving()) {
+            switch (unit.getDirection()) {
+                case UP:
+                    unit.getBounds().y += unit.getSpeed().y;
+                    if ((int) unit.getBounds().y % GameDimensions.unitHeight == 0)
+                        unit.setMoving(false);
+                    break;
+                case DOWN:
+                    unit.getBounds().y -= unit.getSpeed().y;
+                    if ((int) unit.getBounds().y % GameDimensions.unitHeight == 0)
+                        unit.setMoving(false);
+                    break;
+                case LEFT:
+                    unit.getBounds().x -= unit.getSpeed().x;
+                    if ((int) unit.getBounds().x % GameDimensions.unitWidth == 0)
+                        unit.setMoving(false);
+                    break;
+                case RIGHT:
+                    unit.getBounds().x += unit.getSpeed().x;
+                    if ((int) unit.getBounds().x % GameDimensions.unitWidth == 0)
+                        unit.setMoving(false);
+                    break;
             }
-            else if (unit.isMoving()) {
-
-                if(unit instanceof Enemy) ((Enemy)unit).setNumberOfBlocksMoved((int)(((Enemy)unit).getNumberOfBlocksMoved() + unit.getSpeed().x));
-                switch (unit.getDirection()) {
-                    case UP: unit.getBounds().y += unit.getSpeed().y;
-                             if ((int)unit.getBounds().y%GameDimensions.unitHeight == 0)
-                                 unit.setMoving(false);
-                             break;
-                    case DOWN:  unit.getBounds().y -= unit.getSpeed().y;
-                             if ((int)unit.getBounds().y%GameDimensions.unitHeight == 0)
-                                 unit.setMoving(false);
-                             break;
-                    case LEFT: unit.getBounds().x -= unit.getSpeed().x;
-                             if ((int)unit.getBounds().x%GameDimensions.unitWidth == 0)
-                                 unit.setMoving(false);
-                             break;
-                    case RIGHT:  unit.getBounds().x += unit.getSpeed().x;
-                             if ((int)unit.getBounds().x%GameDimensions.unitWidth == 0)
-                                 unit.setMoving(false);
-                             break;
-                }
-
-            }
-
+            if (unit instanceof Enemy)
+                ((Enemy) unit).setNumberOfBlocksMoved((int) (((Enemy) unit).getNumberOfBlocksMoved() + unit.getSpeed().x));
         }
     }
-
-    private void startDeathTimer(Unit unit){
-        unit.setDeathTimer(TimeUtils.millis());
-        unit.setDeathTimerStarted(true);
-    }
-
 }
+
