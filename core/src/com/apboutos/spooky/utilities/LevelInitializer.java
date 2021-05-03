8f2fa362f21cd6732f9a62acbad69b02cc681ed1
exophrenik <exophrenik@gmail.com>
2021-05-03 13:33:07 +0300

    General refactoring.

--- core/src/com/apboutos/spooky/utilities/LevelInitializer.java
+++ core/src/com/apboutos/spooky/utilities/LevelInitializer.java
@@ -1,15 +1,9 @@
 package com.apboutos.spooky.utilities;
 
-import com.apboutos.spooky.boot.Spooky;
-import com.apboutos.spooky.units.block.Bouncing;
+import com.apboutos.spooky.units.Block;
+import com.apboutos.spooky.units.Enemy;
 import com.apboutos.spooky.units.Unit;
-import com.apboutos.spooky.units.block.Diamond;
-import com.apboutos.spooky.units.block.Dynamite;
-import com.apboutos.spooky.units.block.Standard;
-import com.apboutos.spooky.units.enemy.Fish;
-import com.apboutos.spooky.units.enemy.Shark;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -34,13 +28,13 @@ public class LevelInitializer {
      * of the specified level.
      */
     @SuppressWarnings("ConstantConditions")
-    public static List<Unit> initializeUnits(int level, Spooky spooky)
+    public static List<Unit> initializeUnits(int levelID)
     {
         List<Unit> units = new ArrayList<>();
         Document document = null;
-        FileHandle handle = Gdx.files.internal("Levels/Level " + level + ".xml");
+        //TODO Filepath loading must be resolved to work for android and ios as well.
         try {
-            document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("C:\\Users\\exoph\\Documents\\Java Projects\\Spooky\\android\\assets\\Levels/Level " + level + ".xml");
+            document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("C:\\Users\\exoph\\Documents\\Java Projects\\Spooky\\android\\assets\\Levels/Level " + levelID + ".xml");
         } catch (ParserConfigurationException | IOException | SAXException e) {
             Gdx.app.error(TAG,e.getMessage(),e);
         }
@@ -55,7 +49,7 @@ public class LevelInitializer {
                 String y = element.getElementsByTagName("y").item(0).getTextContent();
                 String type = element.getElementsByTagName("type").item(0).getTextContent();
 
-                units.add(createNewUnit(x,y,type, spooky));
+                units.add(createNewUnit(x,y,type));
             }
         }
         return units;
@@ -64,17 +58,17 @@ public class LevelInitializer {
     /**
      * Creates a new Unit from the given parameters.
      */
-    private static Unit createNewUnit(String x, String y, String type, Spooky spooky){
+    private static Unit createNewUnit(String x, String y, String type){
 
         switch (type){
-            case "Standard":    return new Standard(Integer.parseInt(x),Integer.parseInt(y), spooky.batch, BlockType.Standard);
-            case "Bouncing":    return new Bouncing(Integer.parseInt(x),Integer.parseInt(y), spooky.batch, BlockType.Bouncing);
-            case "BigBouncing": return new Bouncing(Integer.parseInt(x),Integer.parseInt(y), spooky.batch, BlockType.BigBouncing);
-            case "Dynamite":    return new Dynamite(Integer.parseInt(x),Integer.parseInt(y), spooky.batch, BlockType.Dynamite);
-            case "BigDynamite": return new Dynamite(Integer.parseInt(x),Integer.parseInt(y), spooky.batch, BlockType.BigDynamite);
-            case "Diamond":     return new Diamond(Integer.parseInt(x),Integer.parseInt(y), spooky.batch, BlockType.Diamond);
-            case "Fish":        return new Fish(Float.parseFloat(x),Float.parseFloat(y));
-            case "Shark":       return new Shark(Float.parseFloat(x),Float.parseFloat(y));
+            case "Standard":    return new Block(Integer.parseInt(x),Integer.parseInt(y),BlockType.Standard);
+            case "Bouncing":    return new Block(Integer.parseInt(x),Integer.parseInt(y),BlockType.Bouncing);
+            case "BigBouncing": return new Block(Integer.parseInt(x),Integer.parseInt(y),BlockType.BigBouncing);
+            case "Dynamite":    return new Block(Integer.parseInt(x),Integer.parseInt(y),BlockType.Dynamite);
+            case "BigDynamite": return new Block(Integer.parseInt(x),Integer.parseInt(y),BlockType.BigDynamite);
+            case "Diamond":     return new Block(Integer.parseInt(x),Integer.parseInt(y),BlockType.Diamond);
+            case "Fish":        return new Enemy(Float.parseFloat(x),Float.parseFloat(y),EnemyType.Fish);
+            case "Shark":       return new Enemy(Float.parseFloat(x),Float.parseFloat(y),EnemyType.Shark);
             default:            throw new IllegalStateException("Type does not match with any known types");
         }
     }
